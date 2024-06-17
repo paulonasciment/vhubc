72f648dcc70c6a021df9b2d14786d0236437cec2
paulonasciment <77516418+paulonasciment@users.noreply.github.com>
2024-06-17 20:10:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import streamlit as st
 import random
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from datetime import datetime, timedelta
 
 produtos_e_servicos = ["Mascarenhas - Holding","Mascarenhas - Back Office","VHUB Gestao - BPO","VMG - Consultorias","C&G - Societário",
